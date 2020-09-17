878019e6653ff579b61357c91894fecb226a047c
Trick <petropatrickj@gmail.com>
2020-09-17 14:54:09 -0400

    Adding Trade Resource Logic scripts

--- scripts.js
+++ scripts.js
@@ -14,59 +14,81 @@ $(document).ready(function(){
     // Click Events
         // add
         $(".add-btn").click(function(){
-            console.log("add")
             var ID =  $(this).attr("id")
             var QuantityLabelID = ID + "lblQuantity"
             var QuantityLabel = document.getElementById(QuantityLabelID)
 
+            console.log("add")
             QuantityLabel.value ++
             goldStatus()
         })
         // Subtract
         $(".minus-btn").click(function(){
-            console.log("minus")
             var ID =  $(this).attr("id")
             var QuantityLabelID = ID + "lblQuantity"
             var QuantityLabel = document.getElementById(QuantityLabelID)
-
+            
+            console.log("minus")
             QuantityLabel.value --
             goldStatus()
         })
         // Trade
         $(".trade-btn").click(function trade(){
-            console.log("trade")
+            var TradeModal = $("#TradeModalBody")
             // Trades any Resource for Gold - 2 for 1 ratio
             if ($(this).attr("data-id") !== "Gold"){
                 var Goldlbl = document.getElementById("GoldlblQuantity")
-                var ID =  $(this).attr("id")
+                var ID = $(this).attr("id")
                 var QuantityLabelID = ID + "lblQuantity"
                 var QuantityLabel = document.getElementById(QuantityLabelID)
 
-                Goldlbl.value ++
-                QuantityLabel.value -=2
+                if (QuantityLabel.value < 2){
+                    alert("Insufficent resources to trade")
+                }
+                    else {
+                        Goldlbl.value ++
+                        QuantityLabel.value -=2
+                        console.log("Trade Resource For Gold")
+                    }
                 goldStatus()
             }
             // Trades Gold for any Resource - 2 for 1 ratio
             else if ($(this).attr("data-id") == "Gold"){
-                //var TradeModal = $("#TradeModalBody")
-                var ID =  $(this).attr("id")
+                var ID = $(this).attr("id")
                 var QuantityLabelID = ID + "lblQuantity"
                 var QuantityLabel = document.getElementById(QuantityLabelID)
+                console.log("Trade Gold For Resource")
                 
-                // TradeModal.empty()
-                // for (i = 0; i < ResourcesArray.length; i++) {
-                //     if ($(this).attr("data-id") !== ResourcesArray[i]){
-                //         var CreateResourceBtn = $("<button>")
-                //         CreateResourceBtn.text(ResourcesArray[i])
-                //         CreateResourceBtn.addClass("btn btn-outline-primary modal-resource-btn") 
-                //         TradeModal.append(CreateResourceBtn)
-                //     }
-                // } 
-                // Goldlbl.value -=2
-                // QuantityLabel.value ++ 
+                TradeModal.empty()
+                for (i = 0; i < ResourcesArray.length; i++) {
+                    if ($(this).attr("data-id") !== ResourcesArray[i]){
+                        var CreateResourceBtn = $("<button>")
+                        CreateResourceBtn.text(ResourcesArray[i])
+                        CreateResourceBtn.addClass("btn btn-outline-primary trade-modal-resource-btn")
+                        CreateResourceBtn.attr("data-target", ResourcesArray[i])  
+                        TradeModal.append(CreateResourceBtn)
+                    }
+                } 
             }
+             // Modal Trade Gold For Resource
+            $(".trade-modal-resource-btn").click(function modalTrade(){
+                var Goldlbl = document.getElementById("GoldlblQuantity")
+                var DataTarget = $(this).attr("Data-Target")
+                var QuantityLabelID = DataTarget + "lblQuantity"
+                var QuantityLabel = document.getElementById(QuantityLabelID)
+
+                if (Goldlbl.value < 2){
+                    alert("Insufficent resources to trade")
+                }
+                else {
+                    Goldlbl.value -=2
+                    QuantityLabel.value ++ 
+                }
+                // TradeModal.toggle()
+            })
         })
-        // Runs goldStatus function when Gold Label focus is lost
+    
+        // Runs goldStatus function when cursor / keyboard focus on Gold Label is lost
         $("#GoldlblQuantity").focusout(function() {
             goldStatus()
         })
@@ -89,46 +111,40 @@ function createResourceCards() {
         Row = document.getElementById("row")
         Column = document.createElement("div")
         Column.setAttribute("class", "column")
-
         // Resource Image
         Img = document.createElement("img")
         Img.setAttribute("class", "thumbnail")
-        Img.setAttribute("src", "images/" + resources[i].name + "Icon.png")
-
+        //Img.setAttribute("src", "images/" + resources[i].name + "Icon.png")
         // Resource Quantity
         Input = document.createElement("input")
         Input.setAttribute("id", resources[i].name + "lblQuantity")
         Input.setAttribute("class", "resource-quantity-lbl")
         Input.setAttribute("data-id", resources[i].name)
         Input.setAttribute("value", resources[i].amount)
-
         // Resource Label
         Header = document.createElement("h5")
         Header.setAttribute("id", resources[i].name + "Header")
         Header.innerHTML = resources[i].name
-
         // Add Button
         AddBtn = document.createElement("button")
         AddBtn.setAttribute("id", resources[i].name)
         AddBtn.setAttribute("class", "btn btn-success add-btn")
-        //AddBtn.setAttribute("onClick", this.proto.add())
         AddBtn.innerText = "+"
-
         // Subtract Button
         SubtractBtn = document.createElement("button")
         SubtractBtn.setAttribute("id", resources[i].name)
         SubtractBtn.setAttribute("class", "btn btn-danger minus-btn")
         SubtractBtn.innerText = "-"
-    
         // Trade Button
         TradeBtn = document.createElement("button")
         TradeBtn.setAttribute("id", resources[i].name)
         TradeBtn.setAttribute("class", "btn btn-outline-primary trade-btn")
-        TradeBtn.setAttribute("data-toggle","modal")
-        TradeBtn.setAttribute("data-target","#TradeModalCenter")
         TradeBtn.setAttribute("data-id", resources[i].name)
-        TradeBtn.innerText = "T"
-       
+        TradeBtn.innerText = "Trade"
+        if (resources[i].name === "Gold"){
+            TradeBtn.setAttribute("data-toggle","modal")
+            TradeBtn.setAttribute("data-target","#TradeModalCenter")
+        }
         // Appends
         Header.append(Input)
         Row.append(Column)
